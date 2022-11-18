52400383957a7a26479a8511169ea75e4884dd4a
OmarD <omar.daghestani@colorado.edu>
2022-11-17 19:38:36 -0700

    test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
 // Connect to MongoDb Database
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/alex-salon', 
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect( process.env.MONGODB_URI, {
+    useNewUrlParser: true
+    
 })
+
+
+
