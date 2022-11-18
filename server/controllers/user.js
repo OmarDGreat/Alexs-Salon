7b5ba585e98f1f67d8b71c7abab3c82c6dcbf725
OmarD <omar.daghestani@colorado.edu>
2022-11-17 19:20:52 -0700

    test

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/user");
+const User = require("../models/User");
 const auth = require("../middleware/auth");
 const router = new express.Router();
 
